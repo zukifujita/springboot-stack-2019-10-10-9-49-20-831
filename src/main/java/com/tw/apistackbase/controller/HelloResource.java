39c1617c471a17a1d535ce2467a7e254b8799fdf
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-15 21:33:00 +0800

    Added HelloResource.java

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,27 +1,33 @@
 package com.tw.apistackbase.controller;
 
+import com.tw.apistackbase.model.Member;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
-
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
 @RequestMapping("/hello")
-public class HelloResource {
 
+public class HelloResource {
+    private List<Member> memberList = new ArrayList<>();
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
+    @GetMapping(path = "/getMembers", produces = {"application/json"})
+    public ResponseEntity<String> getAll() {
+        String memberCompile = "";
+        for(Member member : memberList){
+            memberCompile += member.getName() + ", \n";
+        }
+        return ResponseEntity.ok("Hello Members: " + memberCompile);
     }
 
-
+    @PostMapping(path = "/members", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> createMember(@RequestBody Member memberFromPost) {
+        memberList.add(memberFromPost);
+        return ResponseEntity.ok("Added Member: " + memberFromPost.getName());
+    }
 }
