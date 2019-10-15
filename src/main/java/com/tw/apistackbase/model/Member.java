4c09d199538015b891f6c58a5d985cb783f5f2fe
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-15 21:35:22 +0800

    Added Model Member.java for HelloResource

---
+++ src/main/java/com/tw/apistackbase/model/Member.java
@@ -0,0 +1,13 @@
+package com.tw.apistackbase.model;
+
+public class Member {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
