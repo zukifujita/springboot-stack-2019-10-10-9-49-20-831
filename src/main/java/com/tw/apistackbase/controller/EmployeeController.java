0509adeb1b9da85cbb3635789e91822e2f90b8fe
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-15 21:33:59 +0800

    Added EmployeeController.java

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private List<Employee> employeeList = new ArrayList<>();
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public ResponseEntity<Stream<Employee>> getEmployeesById(@PathVariable Integer id) {
+        return ResponseEntity.ok(employeeList.stream().filter(element -> element.getId().equals(id)));
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<List<Employee>> createEmployee(@RequestBody Employee employeePost) {
+        employeeList.add(employeePost);
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<List<Employee>> putEmployee(@PathVariable Integer id, @RequestBody Employee employeePut) {
+        Employee employee = employeeList.stream().filter(element -> element.getId().equals(id)).findFirst().orElse(null);
+
+        employee.setName(employeePut.getName());
+        employee.setAge(employeePut.getAge());
+        employee.setGender(employeePut.getGender());
+
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<List<Employee>> deleteEmployee(@PathVariable Integer id) {
+        Employee employee = employeeList.stream().filter(element -> element.getId().equals(id)).findFirst().orElse(null);
+        employeeList.remove(employee);
+        return ResponseEntity.ok(employeeList);
+    }
+}
